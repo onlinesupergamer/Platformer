6477e22763997fa55d36f32c44d5d6c1cceba525
onlinesupergamer <bennyguybills@gmail.com>
2022-07-05 19:54:16 -0600

    Platform

---
+++ Platformer/Assets/Levels/ForestDev/Scripts/Waypoints.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public Transform GetWaypoint(int waypointIndex) 
+    {
+        return transform.GetChild(waypointIndex);
+    }
+
+    public int GetNextWaypointIndex(int currentWaypointIndex) 
+    {
+        int nextWaypointIndex = currentWaypointIndex + 1;
+
+        if (nextWaypointIndex == transform.childCount) 
+        {
+            nextWaypointIndex = 0;
+        }
+
+        return nextWaypointIndex;
+    }
+
+}
